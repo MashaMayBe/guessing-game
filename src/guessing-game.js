5fe7d3cabbb0acefb11986d7d20ab21dac03d325
MashaMayBe <51898319+MashaMayBe@users.noreply.github.com>
2019-10-06 23:10:28 +0300

    Add files via upload

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round(this.min+Math.abs(this.max-this.min)/2);
     }
 
     lower() {
-
+        this.max = Math.round(this.min+Math.abs(this.max-this.min)/2);
     }
 
     greater() {
-
+        this.min += Math.round(Math.abs(this.max-this.min)/2);
     }
 }
 
